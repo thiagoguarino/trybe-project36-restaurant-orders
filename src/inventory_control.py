e554a392dca4a0185a3260bc2482fcb4183db133
Thiago Guarino <thiago.guarino@outlook.com>
2023-03-12 22:57:50 -0300

    refactor: InventoryControl class - task 3 and 4

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -31,40 +31,43 @@ class InventoryControl:
         }
         self.orders = []
 
-    def add_new_order(self, customer, order, day):
-        for i in self.INGREDIENTS[order]:
-            if self.MINIMUM_INVENTORY[i] <= self.ingredients_to_buy[i]:
-                return False
-
-        self.orders.append([customer, order, day])
-
-        ingredients_counter = Counter()
-
-        for order in self.orders:
-            for order_data in order:
-                if order_data in self.INGREDIENTS.keys():
-                    order_ingredients = self.INGREDIENTS[order_data]
-                    for ingredient in order_ingredients:
-                        ingredients_counter[ingredient] += 1
-
-        for ingredient in self.MINIMUM_INVENTORY.keys():
-            if ingredient not in ingredients_counter.keys():
-                ingredients_counter.update({ingredient: 0})
-
-        aa = ingredients_counter.most_common()
-
-        for ingredient_data in aa:
-            self.ingredients_to_buy[ingredient_data[0]] = ingredient_data[1]
-
-    def get_quantities_to_buy(self):
-        return self.ingredients_to_buy
-
     def is_ingredient_available(self, order):
         quant_to_buy = self.get_quantities_to_buy()
         for ingredient in self.INGREDIENTS[order]:
             if quant_to_buy[ingredient] >= self.MINIMUM_INVENTORY[ingredient]:
                 return False
             return True
+
+    def ingredients_counter(self, orders, INGREDIENTS):
+        ingredients_counter = Counter()
+
+        for order in orders:
+            for order_data in order:
+                if order_data in INGREDIENTS.keys():
+                    order_ingredients = INGREDIENTS[order_data]
+                    for ingredient in order_ingredients:
+                        ingredients_counter[ingredient] += 1
+        return ingredients_counter
+
+    def add_new_order(self, customer, order, day):
+        if self.is_ingredient_available(order) is not True:
+            return False
+
+        self.orders.append([customer, order, day])
+
+        counter = self.ingredients_counter(self.orders, self.INGREDIENTS)
+
+        for ingredient in self.MINIMUM_INVENTORY.keys():
+            if ingredient not in counter.keys():
+                counter.update({ingredient: 0})
+
+        ingredients_list_tuples = counter.most_common()
+
+        for ingredient_data in ingredients_list_tuples:
+            self.ingredients_to_buy[ingredient_data[0]] = ingredient_data[1]
+
+    def get_quantities_to_buy(self):
+        return self.ingredients_to_buy
 
     def get_available_dishes(self):
         result = set()
